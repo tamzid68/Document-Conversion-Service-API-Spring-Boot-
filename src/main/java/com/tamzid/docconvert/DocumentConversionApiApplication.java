3c06c2d95b84538fe8a765ab59461e3d80abb6cc
ASM Tamzid <atkthegreat9999@gmail.com>
2025-07-10 01:31:03 +0600

    firs commit

---
+++ src/main/java/com/tamzid/docconvert/DocumentConversionApiApplication.java
@@ -0,0 +1,16 @@
+package com.tamzid.docconvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentConversionApiApplication {
+
+	public static void main(String[] args) {
+		// Starting the Spring Boot application
+		// This will initialize the application context and start the embedded server
+
+		SpringApplication.run(DocumentConversionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamzid/docconvert/DocumentConversionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamzid.docconvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentConversionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
